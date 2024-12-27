497a48eb3a3d4ae3e60d0c0d442c5e7d3ff54d69
Carlos Pinto Guedes <carlospintoguedes@Carloss-MBP.lan>
2024-12-27 13:05:10 +0000

    Small fix

--- storyblok/Page/Page.vue
+++ storyblok/Page/Page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-editable="blok" class="page gap-16 flex flex-col pb-16">
+  <div v-editable="blok" class="page gap-16 flex flex-col pb-16" :class="containerClass">
     <div class="page__component" v-for="blok in blok.body" :key="blok._uid">
       <StoryblokComponent :blok="blok" />
     </div>
@@ -7,11 +7,17 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   blok: {
     type: Object,
     required: true,
   },
+});
+
+const containerClass = computed(() => {
+  return props.blok.body && props.blok.body[0] && props.blok.body[0].component === 'Hero'
+    ? ''
+    : 'pt-16';
 });
 </script>
 
