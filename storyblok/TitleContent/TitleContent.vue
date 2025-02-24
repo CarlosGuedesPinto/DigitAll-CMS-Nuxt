5bef7d3d4264168661ca3f56fb12824c1bb4f1e9
Carlos Pinto Guedes <carlospintoguedes@Carloss-MacBook-Pro.local>
2025-02-24 17:46:05 +0000

    Add hover

--- storyblok/TitleContent/TitleContent.vue
+++ storyblok/TitleContent/TitleContent.vue
@@ -8,7 +8,7 @@
         'title-content__title-wrapper--border': blok.title && blok.content.length > 0
       }">
       <span v-if="blok.title" class="title-content__title ldtech"  v-html="formatText(blok.title)" />
-      <NuxtLink v-if="blok.link" class="extrabold self-end text-right" :to="blok.link.url ? blok.link.url : `/${blok.link.cached_url}`">{{ blok.linkLabel }}</NuxtLink>
+      <NuxtLink v-if="blok.link" class="title-content__link extrabold self-end text-right" :to="blok.link.url ? blok.link.url : `/${blok.link.cached_url}`">{{ blok.linkLabel }}</NuxtLink>
     </div>
     <div v-if="blok.content.length > 0" class="title-content__content">
       <template
